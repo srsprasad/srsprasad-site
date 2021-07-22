a1b049f1fe7b78df5bb90a03dae1e24e4bd94221
Ravi shankar prasad Surthi <srsprasad@gmail.com>
2021-07-23 02:33:45 +0530

    Added some external data load options

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GridOptions } from 'ag-grid-community';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,73 +11,35 @@ import { GridOptions } from 'ag-grid-community';
 export class DashboardComponent implements OnInit {
 
   public gridOptions: GridOptions;
+  public httpClient: HttpClient;
 
-  constructor() {
+  constructor( http: HttpClient ) {
 
+    this.httpClient = http;
     this.gridOptions = this.gridOptions = <GridOptions>{
-      enableSorting: true,
-      enableFilter: true,
       pagination: true
     };
 
-    this.gridOptions.rowData = [
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Toyota', model: 'Celica', price: 35000, status: 'Verified' },
-      { make: 'Ford', model: 'Mondeo', price: 32000, status: 'Uploaded' },
-      { make: 'Porsche', model: 'Boxter', price: 72000, status: 'Submitted' }
-  ];
-
   this.gridOptions.columnDefs = [
-        { field: 'make', sortable: true, filter: true },
-        { field: 'model', sortable: true, filter: true },
-        { field: 'price', sortable: true, filter: true },
-        { field: 'status', sortable: true, filter: true}
+      { field: 'status', headerName: 'Status',resizable: true,sortable: true, filter: true},
+      { field: 'ALUMNI_NAME', headerName: 'Alumni Name',resizable: true, sortable: true, filter: true } ,
+      { field: 'STUDENT_NAME', headerName: 'Student Name', sortable: true,resizable: true, filter: true },
+      { field: 'SCHOOL_ID', headerName:'School ID', sortable: true,resizable: true, filter: 'agNumberColumnFilter' },
+      { field: 'SCHOOL_NAMES', headerName: 'School Name', sortable: true,resizable: true, filter: true }
     ];
-
-    this.gridOptions.rowClassRules = {
-      'orange-rows': function (params) { return params.data.status == 'Submitted'; },
-      'green-rows':  function (params) { return params.data.status === 'Verified'; },
-      'yellow-rows':  function (params) { return params.data.status == 'Uploaded'; }
-    };
   }
 
 
   ngOnInit(): void {
-  }
+    this.httpClient.get<any[]>('http://localhost:8080/student/all').subscribe(data=>{
+      console.log(data);
+      this.gridOptions.rowData = data;
+    });
 
+    this.gridOptions.rowClassRules = {
+      'orange-rows': function (params) { return params.data.status == 'Submitted' },
+      'green-rows':  function (params) { return params.data.status == 'Verified' },
+      'yellow-rows':  function (params) { return params.data.status == 'Uploaded' }
+    };
+  }
 }
